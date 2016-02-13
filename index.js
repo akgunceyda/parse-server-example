67e4607c0bba82b27dc7a61175baf54c9efb6b2a
mehmetcelikel <mehmetcelikel@gmail.com>
2016-02-14 00:44:58 +0200

    config setup

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_dd2q1stl:9jthkrted029tfc6tqon607d0r@ds055905.mongolab.com:55905/heroku_dd2q1stl',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'ceydaParse' //Add your master key here. Keep it secret!
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
